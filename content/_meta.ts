623e082c17dcc67320ecc0b70c0fe8fc5c11ab7a
Aidan <230012360+aidancrosbie@users.noreply.github.com>
2025-12-11 17:20:27 -0800

    Add Docusaurus documentation site

---
+++ content/_meta.ts
@@ -0,0 +1,23 @@
+export default {
+  'index': 'Home',
+  'api-valid-jolli-api-yaml': {
+    title: 'Valid - Jolli API (YAML)',
+    type: 'page',
+    href: '/api-docs-valid-jolli-api-yaml.html'
+  },
+  'api-valid-pet-store-api-yaml': {
+    title: 'Valid - Pet Store API (YAML)',
+    type: 'page',
+    href: '/api-docs-valid-pet-store-api-yaml.html'
+  },
+  'api-valid-jolli-api-json': {
+    title: 'Valid - Jolli API (JSON)',
+    type: 'page',
+    href: '/api-docs-valid-jolli-api-json.html'
+  },
+  'api-valid-pet-store-api-json': {
+    title: 'Valid - Pet Store API (JSON)',
+    type: 'page',
+    href: '/api-docs-valid-pet-store-api-json.html'
+  }
+}
